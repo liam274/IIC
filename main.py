060554203d46fcd9ebdeea209a0168a24b1e6426
adin65 <46980135+adin65@users.noreply.github.com>
2023-01-30 12:23:24 +0800

    debug main.py.

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ CORS(app)
 np.set_printoptions(suppress=True)
 
 # Load the model
-model = load_model("keras_Model.h5", compile=False)
+model = load_model("keras_model.h5", compile=False)
 
 # Load the labels
 class_names = open("labels.txt", "r").readlines()
