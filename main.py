1d3e8c2ea39d83924500f308bd72370a81e2835a
adin65 <46980135+adin65@users.noreply.github.com>
2023-01-30 12:13:27 +0800

    modified main.py.

--- main.py
+++ main.py
@@ -22,42 +22,42 @@ def LoadImage(URL,height=224,width=224):
     return ImageOps.fit(Image.open(BytesIO(requests.get(URL).content)).convert("RGB"),(width,height),Image.Resampling.LANCZOS)
 @app.route("/prediction/", methods=['GET', 'POST'])
 def keras():
-    #Get all the values in your POST request. 
-    image = request.args.get('url')
-    threshold = int(request.args.get("threshold"))
-    #Follow all the neccessary steps to get the prediction of your image. 
-    image = loadImage(image)
-    #turn the image into a numpy array
-    image_array = np.asarray(image)
-    # Normalize the image
-    normalized_image_array = (image_array.astype(np.float32) / 127.0) - 1
-    data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
-    # Load the image into the array
-    data[0] = normalized_image_array
+    #Get all the values in your POST request. 
+    image = request.args.get('url')
+    threshold = int(request.args.get("threshold"))
+    #Follow all the neccessary steps to get the prediction of your image. 
+    image = loadImage(image)
+    #turn the image into a numpy array
+    image_array = np.asarray(image)
+    # Normalize the image
+    normalized_image_array = (image_array.astype(np.float32) / 127.0) - 1
+    data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
+    # Load the image into the array
+    data[0] = normalized_image_array
 
-    # run the inference
-    prediction = model.predict(data)
-    # if prediction.argmax() >= threshold/100:
-    #     Id, recognized_object = labels[prediction.argmax()] # the label that corresponds to highest prediction
-    # else:
-    #     recognized_object = "unknown"
+    # run the inference
+    prediction = model.predict(data)
+    # if prediction.argmax() >= threshold/100:
+    #     Id, recognized_object = labels[prediction.argmax()] # the label that corresponds to highest prediction
+    # else:
+    #     recognized_object = "unknown"
 
-    index = np.argmax(prediction)
-    class_name = class_names[index]
-    confidence_score = prediction[0][index]
-    print(index, class_name, confidence_score, prediction)
-    if confidence_score >= threshold/100:
-        return class_name, 200
-    else:
-        return "not known", 200
-    #Return the prediction and a 200 status
-    #return recognized_object, 200
+    index = np.argmax(prediction)
+    class_name = class_names[index]
+    confidence_score = prediction[0][index]
+    print(index, class_name, confidence_score, prediction)
+    if confidence_score >= threshold/100:
+        return class_name, 200
+    else:
+        return "not known", 200
+    #Return the prediction and a 200 status
+    #return recognized_object, 200
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!', 200
+    return 'Hello, World!', 200
 if __name__ == '__main__':
-    app.run(debug=True, port=os.getenv("PORT", default=5000))
+    app.run(debug=True, port=os.getenv("PORT", default=5000))
 """
 # Create the array of the right shape to feed into the keras model
 # The 'length' or number of images you can put into the array is
